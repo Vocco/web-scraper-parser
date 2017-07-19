61aefe41b4191a49cac2b5bad5e58fdc6982b407
Vojtech Krajnansky <vojtech.krajnansky@gmail.com>
2017-07-19 19:45:31 +0200

    docs(MutableInt): added explanation for MutableInt usage

--- src/main/java/vkrajn/scraper/utils/MutableInt.java
+++ src/main/java/vkrajn/scraper/utils/MutableInt.java
@@ -1,33 +1,35 @@
 package vkrajn.scraper.utils;
 
 /**
- * An integer class which is initialized to 1, can never contain null.
- * 
+ * An integer class which is initialized to 1, can never contain null. Used for
+ * performance purposes, see
+ * https://stackoverflow.com/questions/81346/most-efficient-way-to-increment-a-map-value-in-java
+ *
  * @author Vojtech Krajnansky
  * @version 07/19/2017
  */
 public class MutableInt {
-        
-        // Attributes
-        private int value;
 
-        // Constructors
-        public MutableInt() {
-            this.value = 1;
-        }
+    // Attributes
+    private int value;
 
-        // Getters
-        public int get() {
-            return value;
-        }
-        
-        // Setters
-        public void set(int value) {
-            this.value = value;
-        }
-        
-        // Public Methods
-        public void increment() {
-            ++value;
-        }
+    // Constructors
+    public MutableInt() {
+        this.value = 1;
     }
+
+    // Getters
+    public int get() {
+        return value;
+    }
+
+    // Setters
+    public void set(int value) {
+        this.value = value;
+    }
+
+    // Public Methods
+    public void increment() {
+        ++value;
+    }
+}
